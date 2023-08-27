4657253df0c78908e0a2be0ef316ec2738a49dae
MooMan <36575231+Namesnipes@users.noreply.github.com>
2023-08-26 22:04:28 -0700

    Open links in a new tab

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ function ItemRow(props) {
   const url = `https://www.saveonfoods.com/sm/pickup/rsid/2226/product/${props.sku}`;
   return (
     <tr>
-      <td><a href={url}>{props.name}</a></td>
+      <td><a href={url} target="_blank" rel="noreferrer">{props.name}</a></td>
       <td>{props.ppd}</td>
       <td>{props.min_spend}</td>
       <td><img src={props.img} alt="what" /></td>
